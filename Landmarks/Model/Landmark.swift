4726e7a12f6450bad5184623fdb75699f461d59b
foxster-mp4 <foxster.utube@gmail.com>
2022-12-28 23:02:24 -0800

    Working interactive featured landmarks

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -29,6 +29,9 @@ struct Landmark: Hashable, Codable, Identifiable {
     var image: Image {
         Image(imageName)
     }
+    var featuredImage: Image? {
+        isFeatured ? Image("\(imageName)_feature") : nil
+    }
     
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,54 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Huy Bui on 2022-12-28.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featuredImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay {
+                TextOverlay(landmark: landmark)
+            }
+    }
+}
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var gradient: LinearGradient {
+        // Darker region at the bottom of the image that fades out towards the center.
+        .linearGradient(
+            Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            gradient
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().landmarks[0])
+    }
+}
